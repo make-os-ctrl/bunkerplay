fe85b913460b3fcc4b382a2ffa3c7b76729ae9f3
make-os-ctrl <lauraiglesias857@gmail.com>
2025-06-02 15:08:12 +0200

    Update install_apache2.sh

--- install_apache2/install_apache2.sh
+++ install_apache2/install_apache2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set +x # to test stderr output in /var/log/killercoda
+set -x # to test stderr output in /var/log/killercoda
 
 echo starting... # to test stdout output in /var/log/killercoda
 
