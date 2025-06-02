7b5842c0155df6d003736bf5555f38ab7a3da76e
make-os-ctrl <lauraiglesias857@gmail.com>
2025-06-02 14:17:47 +0200

    Update install_apache2.sh

--- install_apache2/install_apache2.sh
+++ install_apache2/install_apache2.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-set -x 
-echo starting ....
-sleep 5 
-# Mettez à jour la liste des paquets
-echo "Mise à jour de la liste des paquets..."
-sudo apt update
+# Mettre à jour la liste des paquets
+apt-get update
 
-# Installez Apache2
-echo "Installation d'Apache2..."
-sudo apt install -y apache2
-
-# Activez Apache2 pour qu'il démarre au démarrage du système
-echo "Activation d'Apache2 pour qu'il démarre automatiquement..."
-sudo systemctl enable apache2
-
-# Démarrez le service Apache2 immédiatement
-echo "Démarrage du service Apache2..."
-sudo systemctl start apache2
-
-# Vérifiez le statut du service Apache2
-echo "Vérification du statut du service Apache2..."
-sudo systemctl status apache2
-
-echo "Installation complète d'Apache2 sur Ubuntu."
-
+# Installer Apache2
+apt-get install -y apache2
 
