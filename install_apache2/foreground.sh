9632ef2d068800bef0e761ef9c82780bcfad6b90
make-os-ctrl <lauraiglesias857@gmail.com>
2025-06-03 13:05:16 +0200

    Update foreground.sh

--- install_apache2/foreground.sh
+++ install_apache2/foreground.sh
@@ -1,4 +1,4 @@
-docker run -d --name bunkerweb-aio -v bw-storage:/data -p 7000:7000/tcp -p 443:8443/tcp -p 443:8443/udp -e ADMIN_USERNAME=admin -e ADMIN_PASSWORD=P@ssw0rd123! -e CHECK_PRIVATE_IP=no/bunkerity/bunkerweb-all-in-one:1.6.2-rc2
+docker run -d --name bunkerweb-aio -v bw-storage:/data -p 7000:7000/tcp -p 443:8443/tcp -p 443:8443/udp -e ADMIN_USERNAME=admin -e ADMIN_PASSWORD=P@ssw0rd123! -e CHECK_PRIVATE_IP=no bunkerity/bunkerweb-all-in-one:1.6.2-rc2
 systemctl start docker
 docker ps
 
