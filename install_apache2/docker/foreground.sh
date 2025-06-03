5a14b3ae463dc65709a49237fdded56b314121fc
make-os-ctrl <lauraiglesias857@gmail.com>
2025-06-03 10:09:59 +0200

    Update foreground.sh

--- install_apache2/docker/foreground.sh
+++ install_apache2/docker/foreground.sh
@@ -1,6 +1,2 @@
-   # Commande pour démarrer le service Docker
-   systemctl start docker
 
-   # Vérifier que Docker fonctionne
-   docker ps
 docker run -d --name bunkerweb-aio -v bw-storage:/data -p 80:8080/tcp -p 443:8443/tcp -p 443:8443/udp bunkerity/bunkerweb-all-in-one:1.6.2-rc2
