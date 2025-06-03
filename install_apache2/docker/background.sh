2f8071045bb22861860b2ba417b0585c17174fd5
make-os-ctrl <lauraiglesias857@gmail.com>
2025-06-03 10:12:20 +0200

    Update background.sh

--- install_apache2/docker/background.sh
+++ install_apache2/docker/background.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+systemctl start docker
 docker run -d --name bunkerweb-aio -v bw-storage:/data -p 80:8080/tcp -p 443:8443/tcp -p 443:8443/udp bunkerity/bunkerweb-all-in-one:1.6.2-rc2
+docker ps
